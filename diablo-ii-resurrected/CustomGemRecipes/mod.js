96c0ddc412a75e14c37b596f249f35421a541744
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-06-17 13:19:32 +0200

    Fixed CustomGemRecipes mod.

--- diablo-ii-resurrected/CustomGemRecipes/mod.js
+++ diablo-ii-resurrected/CustomGemRecipes/mod.js
@@ -1,32 +1,24 @@
 const cubemainFilename = 'global\\excel\\cubemain.txt';
 const cubemain = D2RMM.readTsv(cubemainFilename);
 
-const gemTiers = [
-  { name: 'Chipped', id: 'c' },
-  { name: 'Flawed', id: 'f' },
-  { name: 'Standard', id: 's' },
-  { name: 'Flawless', id: 'z' },
-  { name: 'Perfect', id: 'p' },
-];
+const gemTiers = ['Chipped', 'Flawed', 'Standard', 'Flawless', 'Perfect'];
 const gemTypes = [
-  { name: 'Amethyst', prefix: 'g', suffix: 'v' },
-  { name: 'Ruby', prefix: 'g', suffix: 'r' },
-  { name: 'Sapphire', prefix: 'g', suffix: 'b' },
-  { name: 'Topaz', prefix: 'g', suffix: 'y' },
-  { name: 'Emerald', prefix: 'g', suffix: 'g' },
-  { name: 'Diamond', prefix: 'g', suffix: 'w' },
-  { name: 'Skull', prefix: 's', suffix: 'k' },
+  { name: 'Amethyst', tiers: ['gcv', 'gfv', 'gsv', 'gzv', 'gpv'] },
+  { name: 'Ruby', tiers: ['gcr', 'gfr', 'gsr', 'glr', 'gpr'] },
+  { name: 'Sapphire', tiers: ['gcb', 'gfb', 'gsb', 'glb', 'gpb'] },
+  { name: 'Topaz', tiers: ['gcy', 'gfy', 'gsy', 'gly', 'gpy'] },
+  { name: 'Emerald', tiers: ['gcg', 'gfg', 'gsg', 'glg', 'gpg'] },
+  { name: 'Diamond', tiers: ['gcw', 'gfw', 'gsw', 'glw', 'gpw'] },
+  { name: 'Skull', tiers: ['skc', 'skf', 'sku', 'skl', 'skz'] },
 ];
 
 const getItemName = (gemTier, gemType) => {
-  return gemType.name === 'Skull'
-    ? `${gemType.prefix}${gemType.suffix}${gemTier.id}`
-    : `${gemType.prefix}${gemTier.id}${gemType.suffix}`;
+  return gemType.tiers[gemTier];
 };
 
 const pushUpgradeRecipe = (oldTier, newTier, gemType) => {
   cubemain.rows.push({
-    description: `${config.gemUpgradeCount}x ${oldTier.name} ${gemType.name} -> ${newTier.name} ${gemType.name}`,
+    description: `${config.gemUpgradeCount}x ${gemTiers[oldTier]} ${gemType.name} -> ${gemTiers[newTier]} ${gemType.name}`,
     enabled: 1,
     version: 100,
     numinputs: config.gemUpgradeCount,
@@ -42,7 +34,7 @@ const pushDowngradeRecipe = (oldTier, newTier, gemType) => {
   const outputItemName = getItemName(newTier, gemType);
 
   cubemain.rows.push({
-    description: `${oldTier.name} ${gemType.name} -> ${config.gemDowngradeCount}x ${newTier.name} ${gemType.name}`,
+    description: `${gemTiers[oldTier]} ${gemType.name} -> ${config.gemDowngradeCount}x ${gemTiers[newTier]} ${gemType.name}`,
     enabled: 1,
     version: 100,
     numinputs: 1,
@@ -55,23 +47,26 @@ const pushDowngradeRecipe = (oldTier, newTier, gemType) => {
 };
 
 gemTypes.forEach((gemType) => {
+  if (config.gemUpgradeCount !== 3) {
+    /* remove default recipes when upgrade count is not 3 */
+    cubemain.rows = cubemain.rows.filter(
+      (row) =>
+        !row.description.match(
+          /3 .* -> .* (Amethyst|Ruby|Sapphire|Topaz|Emerald|Diamond|Skull)/
+        )
+    );
+  }
+
   for (let i = 0; i < gemTiers.length; i++) {
-    if (i > 0) {
+    if (config.enableDowngrade && i > 0) {
       /* add custom recipes */
-      pushDowngradeRecipe(gemTiers[i], gemTiers[i - 1], gemType);
+      pushDowngradeRecipe(i, i - 1, gemType);
     }
 
-    /* skip upgrade recipes if gem count is 3, as no change to the default is required */
+    /* skip upgrade recipes if upgrade count is 3, as no change to the default is required */
     if (config.gemUpgradeCount !== 3 && i < gemTiers.length - 1) {
-      /* remove default recipes */
-      cubemain.rows = cubemain.rows.filter(
-        (row) =>
-          !row.description.match(
-            /3 .* -> .* (Amethyst|Ruby|Sapphire|Topaz|Emerald|Diamond|Skull)/
-          )
-      );
       /* add custom recipes */
-      pushUpgradeRecipe(gemTiers[i], gemTiers[i + 1], gemType);
+      pushUpgradeRecipe(i, i + 1, gemType);
     }
   }
 });
