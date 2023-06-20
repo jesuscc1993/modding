b67bd44be04ce6625c39b5f9ac37db13ad029572
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2023-06-20 18:29:32 +0200

    Updated smaller items mod.

--- diablo-ii-resurrected/SmallerItems/mod.js
+++ diablo-ii-resurrected/SmallerItems/mod.js
@@ -1,17 +1,54 @@
-const processFile = (filename) => {
+const scale = 0.4;
+
+const GroupName = {
+  Armor: 'armor',
+  Misc: 'misc',
+  Weapon: 'weapon',
+};
+
+const shouldScaleItem = (item, groupName) => {
+  return (
+    item.invwidth * item.invheight > 2 ||
+    (groupName === GroupName.Armor && item.belt)
+  );
+};
+
+const processTable = (filename, groupName) => {
   const contents = D2RMM.readTsv(filename);
   contents.rows.forEach((row) => {
-    const w = Math.ceil(row.invwidth * config.scale);
-    const h = Math.ceil(row.invheight * config.scale);
-    const maxSockets = w * h;
+    if (shouldScaleItem(row, groupName)) {
+      const w = Math.ceil(row.invwidth * scale);
+      const h = Math.ceil(row.invheight * scale);
+      const maxSockets = w * h;
 
-    row.invwidth = w;
-    row.invheight = h;
-    row.gemsockets = row.gemsockets > maxSockets ? maxSockets : row.gemsockets;
+      row.invwidth = w;
+      row.invheight = h;
+      row.gemsockets =
+        row.gemsockets > maxSockets ? maxSockets : row.gemsockets;
+    }
   });
   D2RMM.writeTsv(filename, contents);
 };
 
-if (config.armor) processFile('global\\excel\\armor.txt');
-if (config.weapons) processFile('global\\excel\\weapons.txt');
-if (config.misc) processFile('global\\excel\\misc.txt');
+const processSprites = (groupName) => {
+  D2RMM.copyFile(
+    `hd\\global\\ui\\items\\${groupName}`,
+    `hd\\global\\ui\\items\\${groupName}`,
+    true
+  );
+};
+
+const processGroup = (filePath, groupName) => {
+  processTable(filePath, groupName);
+  processSprites(groupName);
+};
+
+if (config.armor) {
+  processGroup('global\\excel\\armor.txt', GroupName.Armor);
+}
+if (config.weapons) {
+  processGroup('global\\excel\\weapons.txt', GroupName.Weapon);
+}
+if (config.misc) {
+  processGroup('global\\excel\\misc.txt', GroupName.Misc);
+}
