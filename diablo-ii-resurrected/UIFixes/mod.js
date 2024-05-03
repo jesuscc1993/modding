b8d5f0f62bfb33ed8be5e1b883abf082368887b6
Caedendi <Caedendi@users.noreply.github.com>
2024-05-03 22:36:51 +0200

    added settings toggles

--- diablo-ii-resurrected/UIFixes/mod.js
+++ diablo-ii-resurrected/UIFixes/mod.js
@@ -1,25 +1,44 @@
-const processLayout = (filename, oldCoords, newCoords) => {
-  const layout = D2RMM.readJson(filename);
-  layout.children.forEach((child) => {
-    if (child.name === 'grid') {
-      const { rect } = child.fields;
-      if (rect.x === oldCoords.x && rect.y === oldCoords.y) {
-        child.fields.rect.x = newCoords.x;
-        child.fields.rect.y = newCoords.y;
-      }
+class ModConstants {
+  static vendorPanelLayoutHd = { 
+    file: `vendorpanellayouthd`,
+    oldCoords: { x: 91, y: 233 },
+    newCoords: { x: 88, y: 235 },
+  };
+
+  static bankExpansionLayoutHd = {
+    file: `bankexpansionlayouthd`,
+    oldCoords: { x: 54, y: 177 },
+    newCoords: { x: 57, y: 177 },
+  };
+}
+
+class UiFixesMod {
+  build() {
+    if (config.shouldFixVendorPanelLayoutHd) {
+      this.processLayout(ModConstants.vendorPanelLayoutHd.file, ModConstants.vendorPanelLayoutHd.oldCoords, ModConstants.vendorPanelLayoutHd.newCoords);
     }
-  });
-  D2RMM.writeJson(filename, layout);
-};
 
-processLayout(
-  'global\\ui\\layouts\\vendorpanellayouthd.json',
-  { x: 91, y: 233 },
-  { x: 88, y: 235 }
-);
+    if (config.shouldFixBankExpansionLayoutHd) {
+      this.processLayout(ModConstants.bankExpansionLayoutHd.file, ModConstants.bankExpansionLayoutHd.oldCoords, ModConstants.bankExpansionLayoutHd.newCoords);
+    }
+  }
 
-processLayout(
-  'global\\ui\\layouts\\bankexpansionlayouthd.json',
-  { x: 54, y: 177 },
-  { x: 57, y: 177 }
-);
+  processLayout(fileName, oldCoords, newCoords) {
+    let path = `global\\ui\\layouts\\${fileName}.json`;
+    let file = D2RMM.readJson(path);
+
+    file.children.forEach((child) => {
+      if (child.name === 'grid') {
+        const { rect } = child.fields;
+        if (rect.x === oldCoords.x && rect.y === oldCoords.y) {
+          child.fields.rect.x = newCoords.x;
+          child.fields.rect.y = newCoords.y;
+        }
+      }
+    });
+
+    D2RMM.writeJson(path, file);
+  };
+}
+
+(new UiFixesMod()).build();
