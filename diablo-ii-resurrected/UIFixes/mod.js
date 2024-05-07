9801f5e5b328b804339008893f184c937a64a920
Jesús Miguel Cruz Cana <jesus.cruz@mimacom.com>
2024-05-07 10:27:10 +0200

    Refactored PR to remove classes.

--- diablo-ii-resurrected/UIFixes/mod.js
+++ diablo-ii-resurrected/UIFixes/mod.js
@@ -1,44 +1,40 @@
-class ModConstants {
-  static vendorPanelLayoutHd = { 
-    file: `vendorpanellayouthd`,
+const Layouts = {
+  VendorPanelLayoutHd: {
+    fileName: `vendorpanellayouthd`,
     oldCoords: { x: 91, y: 233 },
     newCoords: { x: 88, y: 235 },
-  };
-
-  static bankExpansionLayoutHd = {
-    file: `bankexpansionlayouthd`,
+  },
+  BankExpansionLayoutHd: {
+    fileName: `bankexpansionlayouthd`,
     oldCoords: { x: 54, y: 177 },
     newCoords: { x: 57, y: 177 },
-  };
-}
+  },
+};
 
-class UiFixesMod {
-  build() {
-    if (config.shouldFixVendorPanelLayoutHd) {
-      this.processLayout(ModConstants.vendorPanelLayoutHd.file, ModConstants.vendorPanelLayoutHd.oldCoords, ModConstants.vendorPanelLayoutHd.newCoords);
-    }
-
-    if (config.shouldFixBankExpansionLayoutHd) {
-      this.processLayout(ModConstants.bankExpansionLayoutHd.file, ModConstants.bankExpansionLayoutHd.oldCoords, ModConstants.bankExpansionLayoutHd.newCoords);
-    }
+const build = () => {
+  if (config.shouldFixVendorPanelLayoutHd) {
+    processLayout(Layouts.VendorPanelLayoutHd);
   }
+  if (config.shouldFixBankExpansionLayoutHd) {
+    processLayout(Layouts.BankExpansionLayoutHd);
+  }
+};
 
-  processLayout(fileName, oldCoords, newCoords) {
-    let path = `global\\ui\\layouts\\${fileName}.json`;
-    let file = D2RMM.readJson(path);
+const processLayout = ({ fileName, oldCoords, newCoords }) => {
+  let path = `global\\ui\\layouts\\${fileName}.json`;
+  let file = D2RMM.readJson(path);
 
-    file.children.forEach((child) => {
-      if (child.name === 'grid') {
-        const { rect } = child.fields;
-        if (rect.x === oldCoords.x && rect.y === oldCoords.y) {
-          child.fields.rect.x = newCoords.x;
-          child.fields.rect.y = newCoords.y;
-        }
+  file.children.forEach((child) => {
+    if (child.name === 'grid') {
+      const { rect } = child.fields;
+      if (rect.x === oldCoords.x && rect.y === oldCoords.y) {
+        child.fields.rect.x = newCoords.x;
+        child.fields.rect.y = newCoords.y;
       }
-    });
+    }
+  });
 
-    D2RMM.writeJson(path, file);
-  };
-}
+  D2RMM.writeJson(path, file);
+};
 
-(new UiFixesMod()).build();
+build();
