7c17e9d91aee2d1810e2d60bf4ed9a1c2bf1fddd
Jesús Miguel Cruz Cana <jesuscc1993@gmail.com>
2025-03-02 21:30:28 +0100

    Single quotes.

--- cyberpunk-2077/reduced-cyberware-cost/src/reduce-cyberware-cost.py
+++ cyberpunk-2077/reduced-cyberware-cost/src/reduce-cyberware-cost.py
@@ -17,17 +17,17 @@ def generate_list(multiplier):
     new_cost = max(round(cost * multiplier / 100), 1)
     new_tinkerer_cost = (cost - new_cost) * -1
 
-    add_cost(f"Variants.Humanity{cost}Cost_inline", base_costs, new_cost)
-    add_cost(f"Variants.Humanity{cost}CostTinkererVariant_inline", tinkerer_costs, new_tinkerer_cost)
+    add_cost(f'Variants.Humanity{cost}Cost_inline', base_costs, new_cost)
+    add_cost(f'Variants.Humanity{cost}CostTinkererVariant_inline', tinkerer_costs, new_tinkerer_cost)
 
   return base_costs, tinkerer_costs
 
 def add_cost(prefix, cost_dict, new_cost):
-  cost_dict[f"{prefix}1.value"] = new_cost
-  cost_dict[f"{prefix}2.value"] = new_cost
+  cost_dict[f'{prefix}1.value'] = new_cost
+  cost_dict[f'{prefix}2.value'] = new_cost
 
 def write_costs(file, costs):
   for key, value in costs.items():
-    file.write(f"{key}: {value}\n")
+    file.write(f'{key}: {value}\n')
 
 generate_lists()
